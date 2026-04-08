4d48e8fc6bfe6af9229da14b8bbbf87167716941
m-stevenson <mitchell.stevenson@georgebrown.ca>
2026-04-08 18:40:29 -0400

    Add change detector

--- 101186647-lab-test2-comp3133/src/app/components/characterdetails/characterdetails.ts
+++ 101186647-lab-test2-comp3133/src/app/components/characterdetails/characterdetails.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, inject, ChangeDetectorRef  } from '@angular/core';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { HarryPotterService } from '../../services/harrypotter';
 import { Character } from '../../models/character.model';
@@ -18,6 +18,7 @@ export class Characterdetails implements OnInit {
 
   character?: Character;
   loading = false;
+  private cdr = inject(ChangeDetectorRef);
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
@@ -25,9 +26,17 @@ export class Characterdetails implements OnInit {
     if (id) {
       this.loading = true;
       this.hpService.getCharacterById(id).subscribe({
-        next: (data) => {
+         next: (data) => {
+          console.log('full data:', data);
+          console.log('data[0]:', data[0]);
+
           this.character = data[0];
           this.loading = false;
+
+          console.log('character after set:', this.character);
+          console.log('loading after set:', this.loading);
+
+          this.cdr.detectChanges();
         },
         error: (err) => {
           console.error('Error loading character details:', err);
