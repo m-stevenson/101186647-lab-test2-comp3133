57e0de20b8b0cdda5fddda5bd0484e87c38a78ed
m-stevenson <mitchell.stevenson@georgebrown.ca>
2026-04-08 16:15:54 -0400

    Update character details component

--- 101186647-lab-test2-comp3133/src/app/components/characterdetails/characterdetails.ts
+++ 101186647-lab-test2-comp3133/src/app/components/characterdetails/characterdetails.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { HarryPotterService } from '../../services/harrypotter';
+import { Character } from '../../models/character.model';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-characterdetails',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule, MatCardModule, MatButtonModule],
   templateUrl: './characterdetails.html',
-  styleUrl: './characterdetails.css',
+  styleUrl: './characterdetails.css'
 })
-export class Characterdetails {}
+export class Characterdetails implements OnInit {
+  private route = inject(ActivatedRoute);
+  private hpService = inject(HarryPotterService);
+
+  character?: Character;
+  loading = false;
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+
+    if (id) {
+      this.loading = true;
+      this.hpService.getCharacterById(id).subscribe({
+        next: (data) => {
+          this.character = data[0];
+          this.loading = false;
+        },
+        error: (err) => {
+          console.error('Error loading character details:', err);
+          this.loading = false;
+        }
+      });
+    }
+  }
+}
