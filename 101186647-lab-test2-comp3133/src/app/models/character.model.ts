607dcac776e2d3d0afab0695ae10094e12f82934
m-stevenson <mitchell.stevenson@georgebrown.ca>
2026-04-08 15:50:02 -0400

    Create character interface

--- 101186647-lab-test2-comp3133/src/app/models/character.model.ts
+++ 101186647-lab-test2-comp3133/src/app/models/character.model.ts
@@ -1,1 +1,17 @@
-export interface Character {}
+export interface Wand {
+    wood: string;
+    core: string;
+    length: number;
+}
+
+export interface Character {
+    id: string;
+    name: string;
+    house: string;
+    image: string;
+    species: string;
+    wizard: string;
+    ancestry: string;
+    wand: Wand;
+    actor: string;
+}
