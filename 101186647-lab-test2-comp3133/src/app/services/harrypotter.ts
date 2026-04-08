317817bb5c1fa7026a96062948790060b6a40f71
m-stevenson <mitchell.stevenson@georgebrown.ca>
2026-04-08 15:54:41 -0400

    Create fetch service

--- 101186647-lab-test2-comp3133/src/app/services/harrypotter.ts
+++ 101186647-lab-test2-comp3133/src/app/services/harrypotter.ts
@@ -1,6 +1,25 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Character } from '../models/character.model';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Harrypotter {}
+export class HarryPotterService {
+  private http = inject(HttpClient);
+  private apiUrl = 'https://hp-api.onrender.com/api'
+
+  getAllCharacters(): Observable<Character[]> {
+    return this.http.get<Character[]>(`${this.apiUrl}/characters`);
+  }
+
+  getCharactersByHouse(house: string): Observable<Character[]>{
+    return this.http.get<Character[]>(`${this.apiUrl}/characters/house/${house}`)
+  }
+
+  getCharacterById(id: string): Observable<Character[]>{
+    return this.http.get<Character[]>(`${this.apiUrl}/characters/${id}`)
+  }
+
+}
