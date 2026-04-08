cb5c75443287285d2df74d34eb7bd688f673d7ab
m-stevenson <mitchell.stevenson@georgebrown.ca>
2026-04-08 16:00:30 -0400

    Update routes

--- 101186647-lab-test2-comp3133/src/app/app.routes.ts
+++ 101186647-lab-test2-comp3133/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Characterlist } from './components/characterlist/characterlist';
+import { Characterdetails } from './components/characterdetails/characterdetails';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Characterlist},
+    {path: 'character/id', component: Characterdetails},
+    {path: '**', redirectTo: ''},
+];
