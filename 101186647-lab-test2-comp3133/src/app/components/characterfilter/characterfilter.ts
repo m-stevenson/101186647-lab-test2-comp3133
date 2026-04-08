ead567a6651956ad5c06d9eb73b01f242125255c
m-stevenson <mitchell.stevenson@georgebrown.ca>
2026-04-08 16:08:44 -0400

    Update character filtering

--- 101186647-lab-test2-comp3133/src/app/components/characterfilter/characterfilter.ts
+++ 101186647-lab-test2-comp3133/src/app/components/characterfilter/characterfilter.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-characterfilter',
-  imports: [],
+  standalone: true,
+  imports: [MatFormFieldModule, MatSelectModule],
   templateUrl: './characterfilter.html',
   styleUrl: './characterfilter.css',
 })
-export class Characterfilter {}
+export class Characterfilter {
+    @Output() houseSelected = new EventEmitter<string>();
+
+    houses = [
+      "Gryffindor",
+      "Slytherin",
+      "Hufflepuff",
+      "Ravenclaw",
+      '',
+    ];
+
+    onHouseChange(value: string) {
+      this.houseSelected.emit(value)
+    }
+
+}
